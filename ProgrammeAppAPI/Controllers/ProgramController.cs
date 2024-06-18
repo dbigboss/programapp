6035f41420383cbd8585b13e3794bf6d3abe98d6
Godstar <sengodstar@gmail.com>
2024-06-18 19:31:55 +0100

    Initialize project and configure gitignore

---
+++ ProgrammeAppAPI/Controllers/ProgramController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProgrammeAppAPI;
+
+[ApiController]
+[Route("[controller]")]
+public class ProgramController : ControllerBase
+{
+    public ProgramController()
+    {
+        
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        return BadRequest();
+    }
+}
